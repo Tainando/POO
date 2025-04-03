878f8f187587d90a55149a1b00a500514c6683e3
Nando <20nandogomes06@gmail.com>
2025-04-02 21:11:38 -0300

    esconder informações

--- Projetinho - poo/huguinho.js
+++ Projetinho - poo/huguinho.js
@@ -1,16 +1,24 @@
 const prompt = require('prompt-sync')();
 
 class Cliente{
-    nome;
+    #nome;
     idade;
     cpf;
     agencia;
+    getNome (){
+        return this.#nome;
+    }
+
+    setNome(Value){
+        this.#nome = Value
+    }
  }
  let cliente = new Cliente();
 
-cliente.nome = prompt("Digite o nome do Cliente");
+cliente.setNome(prompt("Digite o nome do Cliente"));
 cliente.idade = prompt("Digite a sua idade");
 cliente.cpf = prompt("Digite o seu CPF");
 cliente.agencia = prompt("digite a sua Agencia");
 
 console.log(cliente);
+console.log(cliente.getNome());
