6bce28dc299ded59284e8806d37b9155c0d8a081
Nando <20nandogomes06@gmail.com>
2025-04-02 21:46:08 -0300

    Selular de pobre

---
+++ Naldo Codigos - POO/pelezinho.js
@@ -0,0 +1,16 @@
+const prompt = require("prompt-sync")()
+
+class produtos{
+    produto1;
+    produto2;
+    produto3;
+    Produto4;
+}
+let Setor = new produtos
+
+produto1.preco = prompt("digite o valor do produto")
+produto2.preco = prompt("digite o valor do produto")
+produto3.preco = prompt("digite o valor do produto")
+produto4.preco = prompt("digite o valor do produto")
+
+console.log(Setor);
